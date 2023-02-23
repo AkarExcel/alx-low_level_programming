a699a6f21836c9fa549a7f38fd457d4167a93c0f
Akar Excel <therealakarexcel@gmail.com>
2023-02-23 04:41:29 -0800

    more nested loops

---
+++ 0x04-more_functions_nested_loops/try.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *print_most_numbers - print numbers from 0 to 9 except 2 and 4
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(int size)
+{
+  size=8;
+  int len, bred;
+  for (len = 1; len <= size; len++)
+    {
+    for (bred = 1; bred <= size; bred++)
+      putchar('#');
+    if ((bred == size));
+  putchar('\n');
+    }
+  putchar('\n');
+}
